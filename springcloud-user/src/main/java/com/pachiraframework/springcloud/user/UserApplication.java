d32745cdc8023e037309cfb2b60f7a3595d3eef3
Kevin Wang <wangxuzheng@aliyun.com>
2017-11-06 08:00:52 +0800

    基于eureka集群实现消费者方调用

---
+++ springcloud-user/src/main/java/com/pachiraframework/springcloud/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.pachiraframework.springcloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+}
